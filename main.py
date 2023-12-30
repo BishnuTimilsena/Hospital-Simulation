73d64f40ad1353c11734a5f224ce57d4a30032f2
Bishnu Timilsena <timilsenabsnu@gmail.com>
2023-12-30 12:19:13 +0545

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import simpy
+import random
+from simulation import run_simulation
+
+# Set up simulation environment
+env = simpy.Environment()
+
+# Set up resources
+receptionist = simpy.Resource(env, capacity=1)
+nurse = simpy.Resource(env, capacity=2)
+ed_doctor = simpy.Resource(env, capacity=1)
+acu_doctor = simpy.Resource(env, capacity=1)
+
+# Set up parameter values
+ed_inter = 8
+mean_register = 2
+mean_triage = 5
+mean_ed_assess = 30
+mean_acu_assess = 60
+
+# Number of patients to simulate
+num_patients = 20
+
+# Run the simulation
+run_simulation(env, num_patients, mean_register, mean_triage, mean_ed_assess, mean_acu_assess, receptionist, nurse, ed_doctor, acu_doctor, ed_inter)
